4d50395535db7118a8d2385307960811bfdb9d64
lou-office-ubuntu <louie@lapapa88.com>
2023-06-29 18:03:19 +0800

    working with-databases

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,10 +3,10 @@
         <article>
             <h1>
                 <a href="/posts/{{ $post->slug }}">
-
                     {{ $post->title }}
                 </a>
             </h1>
+            by: <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
             <p>{{ $post->excerpt }}</p>
         </article>
     @endforeach
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Models\Category;
+use App\Models\User;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +19,16 @@ use App\Http\Controllers\PostController;
 
 Route::get('/', [PostController::class, 'index']);
 
-Route::get('posts/{post}', [PostController::class, 'show']);
+Route::get('posts/{post:slug}', [PostController::class, 'show']);
+
+Route::get('categories/{category:slug}', function (Category $category){
+    return View::make('posts', [
+        'posts' => $category->posts
+    ]);
+});
+
+Route::get('authors/{author:username}', function (User $author){
+    return View::make('posts', [
+        'posts' => $author->posts
+    ]);
+});
